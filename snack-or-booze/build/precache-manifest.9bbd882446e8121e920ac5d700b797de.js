66f41cec91df7ff7d29ede412a3736326e620bab
rahul <rahulkapoor07@gmail.com>
2022-02-22 17:51:45 -0800

    all ready to deploy

---
+++ snack-or-booze/build/precache-manifest.9bbd882446e8121e920ac5d700b797de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4cc248bcdd24ea1d8c26b1bd851e5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb5c36a9f8479dad2f3",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "53520a75d8b0384e14ac",
+    "url": "/static/css/main.e9dc50ba.chunk.css"
+  },
+  {
+    "revision": "0cb5c36a9f8479dad2f3",
+    "url": "/static/js/2.92840243.chunk.js"
+  },
+  {
+    "revision": "e317015e6aea0983d75cc5b07d2258fa",
+    "url": "/static/js/2.92840243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53520a75d8b0384e14ac",
+    "url": "/static/js/main.b215c683.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
